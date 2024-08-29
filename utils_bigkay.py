8800070fd47702cd2fdfc66daeb4e0d87ecec391
wkarto <kwomenker14@gmail.com>
2024-08-29 08:47:43 -0500

    "Iteration 1 - custom"

--- utils_bigkay.py
+++ utils_bigkay.py
@@ -1,6 +1,6 @@
 ''' ITERATION 1
 
-Module: Stellar Analytics - Reusable Module for My Data Analytics Projects
+Module: Venirr Analytics - Reusable Module for My Data Analytics Projects
 
 This module provides a simple, reusable foundation for my analytics projects. 
 When we work hard to write useful code, we want it to be reusable.
@@ -28,7 +28,7 @@ I'll test it in an online interpreter to ensure this version runs correctly befo
 # Declare a global variable named byline.
 #####################################
 
-byline: str = 'Stellar Analytics: Delivering Professional Insights'
+byline: str = 'Venirr Analytics: Delivering group'
 
 #####################################
 # Define a main() function for this module.
@@ -46,6 +46,13 @@ byline: str = 'Stellar Analytics: Delivering Professional Insights'
 def main() -> None:
     '''Print the byline to the console when this function is called.'''
     print(byline)
+
+#####################################
+# Conditional Execution - Only call main() when executing this module as a script.
+#####################################
+
+if __name__ == '__main__':
+    main()
 
 #####################################
 # Conditional Execution - Only call main() when executing this module as a script.
