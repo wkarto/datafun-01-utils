0e82f192610d66e999b7f9c82a061f44ddf63a30
wkarto <kwomenker14@gmail.com>
2024-08-29 08:07:45 -0500

    Create utils_bigkay1.py

---
+++ utils_bigkay.py
@@ -0,0 +1,55 @@
+''' ITERATION 1
+
+Module: Stellar Analytics - Reusable Module for My Data Analytics Projects
+
+This module provides a simple, reusable foundation for my analytics projects. 
+When we work hard to write useful code, we want it to be reusable.
+A good byline could be used in every Python analytics project we do.
+
+Process:
+
+We don't write code from top to bottom; instead, we often write it from the outside in.
+Here's what a first draft of my utils_case.py might look like:
+
+1. I start with this docstring at the very beginning.
+   I use it to clarify the purpose of my Python file and organize my thoughts.
+   
+2. I'll declare a global variable for my byline string and just set it to some simple text.
+
+3. I'll declare a main() function for my module. When I run this script, I can use main() to test my byline.
+
+4. I'll add the boilerplate conditional execution code so I only run the main() function when 
+   this script is executed directly (but not when I import it into another file).
+
+I'll test it in an online interpreter to ensure this version runs correctly before continuing.
+'''
+
+#####################################
+# Declare a global variable named byline.
+#####################################
+
+byline: str = 'Stellar Analytics: Delivering Professional Insights'
+
+#####################################
+# Define a main() function for this module.
+#####################################
+
+# Create a function named main.
+# A function is a block of code that performs a specific task.
+# This function will simply print the byline to the console.
+# Add a type hint to indicate that this function doesn't return anything when called 
+# (that is, it has a Python type of None).
+# It doesn't need any additional information passed in, 
+# so there's nothing needed inside the parentheses.
+# Everything afer the colon (:) must be indented (usually 4 spaces)
+
+def main() -> None:
+    '''Print the byline to the console when this function is called.'''
+    print(byline)
+
+#####################################
+# Conditional Execution - Only call main() when executing this module as a script.
+#####################################
+
+if __name__ == '__main__':
+    main()
