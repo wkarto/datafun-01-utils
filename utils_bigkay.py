3b8783db57413fd33c6a895d0d2f428198931097
wkarto <kwomenker14@gmail.com>
2024-08-30 22:03:24 +0530

    Iteration 4 - add stats

--- utils_bigkay.py
+++ utils_bigkay.py
@@ -1,4 +1,4 @@
-''' ITERATION 3
+''' ITERATION 4
 
 Module: Venirr Analytics - Reusable Module for My Data Analytics Projects
 
@@ -7,13 +7,27 @@ When we work hard to write useful code, we want it to be reusable.
 A good byline could be used in every Python analytics project we do.
 
 Process:
-
-In this third iteration, I declare additional variables to show skills with different data types.
+In this fourth iteration, I introduce some basic statistics using Python.
+    - min() is a built in function to find the smallest value passed in
+    - max() is a built in function to find the largest value passed in
+    - The statistics module offers methods to calculate mean and standard deviation.
 '''
 
 #####################################
-# Declare global variables - keep byline at the end 
-# We will use this information in a smarter byline 
+# Import Modules at the Top
+#####################################
+
+# In Python, we can import modules to add extra tools and functions.
+# Below, we're importing:
+# - 'statistics': This gives us tools to calculate things like averages.
+# Use CTRL F and type statistics to see where it is used in the code. 
+# Did you find statistics.mean()?
+# Did you find statistics.stdev()?
+
+import statistics
+
+#####################################
+# Declare global variables
 #####################################
 # Boolean variable to indicate if the company has internation clients
 has_international_clients: bool = False
@@ -27,8 +41,29 @@ skills_offered: list = ["Data Engineering", "Artificial Intelliegnce", "Machine 
 # List of floats representing individual client satisfaction scores
 client_satisfaction_scores: list = [4.7, 5.0, 4.5]
 
+Rating: list = [7.7, 5.7, 9.8]
+
 # List of clients
 clients = ['ABC', 'XYZ', 'IJK']
+
+
+# Calculate Basic Statistics 
+#   Do this BEFORE we declare the byline
+#   so the values have been calculated
+#   and are ready for use in the byline.
+#####################################
+
+#Calculate basic stats using built-in functions min(), max(), and statistics module functions mean() and stdev()
+min_score: float = min(client_satisfaction_scores)
+max_score: float = max(client_satisfaction_scores)
+mean_score: float = statistics.mean(client_satisfaction_scores)
+stdev_score: float = statistics.stdev(client_satisfaction_scores)
+
+min_rating: float = min(Rating)
+max_rating: float = max(Rating)
+mean_rating: float = statistics.mean(Rating)
+stdev_rating: float = statistics.stdev(Rating)
+
 
 #####################################
 # Declare a global variable named byline.
@@ -44,6 +79,17 @@ Years in Operation:             {years_in_operation}
 Clients:     {clients}
 Skills Offered:                 {skills_offered}
 Client Satisfaction Scores: {client_satisfaction_scores}
+Rating: {Rating}
+
+Minimum Client Satisfaction Scores:               {min_score}
+Maximum Client Satisfaction Scores:               {max_score}
+Average Client Satisfaction Scores:               {mean_score:.2f}
+Standard Deviation of Client Satisfaction Scores: {stdev_score:.2f}
+
+Minimum Rating:               {min_rating}
+Maximum Rating:               {max_rating}
+Average Rating:               {mean_rating:.2f}
+Standard Rating: {stdev_rating:.2f}
 
 """
 
