857019087e7dc05052ba21b3c046105f97661a82
wkarto <kwomenker14@gmail.com>
2024-08-30 21:54:32 +0530

    Iteration 3 - multiline byline

--- utils_bigkay.py
+++ utils_bigkay.py
@@ -1,4 +1,4 @@
-''' ITERATION 2
+''' ITERATION 3
 
 Module: Venirr Analytics - Reusable Module for My Data Analytics Projects
 
@@ -8,27 +8,44 @@ A good byline could be used in every Python analytics project we do.
 
 Process:
 
-We don't write code from top to bottom; instead, we often write it from the outside in.
-Here's what a first draft of my utils_case.py might look like:
-
-1. I start with this docstring at the very beginning.
-   I use it to clarify the purpose of my Python file and organize my thoughts.
-   
-2. I'll declare a global variable for my byline string and just set it to some simple text.
-
-3. I'll declare a main() function for my module. When I run this script, I can use main() to test my byline.
-
-4. I'll add the boilerplate conditional execution code so I only run the main() function when 
-   this script is executed directly (but not when I import it into another file).
-
-I'll test it in an online interpreter to ensure this version runs correctly before continuing.
+In this third iteration, I declare additional variables to show skills with different data types.
 '''
 
 #####################################
+# Declare global variables - keep byline at the end 
+# We will use this information in a smarter byline 
+#####################################
+# Boolean variable to indicate if the company has internation clients
+has_international_clients: bool = False
+
+# Integer variable for the number of years in operation
+years_in_operation: int = 4
+
+# List of strings representing the skills offered by the company
+skills_offered: list = ["Data Engineering", "Artificial Intelliegnce", "Machine Learning"]
+
+# List of floats representing individual client satisfaction scores
+client_satisfaction_scores: list = [4.7, 5.0, 4.5]
+
+# List of clients
+clients = ['ABC', 'XYZ', 'IJK']
+
+#####################################
 # Declare a global variable named byline.
+# Make it a multiline f-string to show our information.
 #####################################
 
-byline: str = 'Venirr Analytics: Delivering Professional Insights'
+byline: str = f"""
+---------------------------------------------------
+Venirr Analytics: Delivering Professional Insights
+---------------------------------------------------
+Has International Clients:      {has_international_clients}
+Years in Operation:             {years_in_operation}
+Clients:     {clients}
+Skills Offered:                 {skills_offered}
+Client Satisfaction Scores: {client_satisfaction_scores}
+
+"""
 
 #####################################
 # Define the get_byline() Function.
@@ -54,13 +71,6 @@ def get_byline() -> str:
 def main() -> None:
     '''Print the byline to the console when this function is called.'''
     print(get_byline())
-
-#####################################
-# Conditional Execution - Only call main() when executing this module as a script.
-#####################################
-
-if __name__ == '__main__':
-    main()
 
 #####################################
 # Conditional Execution - Only call main() when executing this module as a script.
